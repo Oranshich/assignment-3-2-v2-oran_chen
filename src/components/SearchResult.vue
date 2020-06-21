506e0872e30864731292e0c403a31cd7f7e296aa
Oran <shichman@post.bgu.ac.il>
2020-06-21 15:08:03 +0300

    -

--- src/components/SearchResult.vue
+++ src/components/SearchResult.vue
@@ -1,16 +1,37 @@
 <template>
-    
+    <b-container>
+        <b-row v-for="{r,index} in recipes" :key="`r.id-${index}`">
+            <b-col>
+                <div v-if="index < 5">
+                    <RecipePreview class="recipePreview" :recipe="r" />
+                </div>
+            </b-col>
+        </b-row>
+    </b-container>
 </template>
 
 <script>
+    import RecipePreview from "./RecipePreview";
     export default {
-        name: "SearchResult",vcom
+        name: "SearchResult",
         data() {
             return {
                 key: value
             }
         },
-
+        components: {
+            RecipePreview
+        },
+        props: {
+            recipes: {
+                type: Array,
+                required: true
+            },
+            num:{
+              type: Number,
+                required: true
+            }
+        }
     }
 </script>
 
--- src/pages/SearchPage.vue
+++ src/pages/SearchPage.vue
@@ -1,33 +1,80 @@
 <template>
-    <div>
-      <div class='is-icon is-searchbox' v-on:click='focus()'></div>
-      <input class='is-field is-searchbox' type='text' ref='input' v-model='entry' />
-      <input class="is-button is-search-button" type="button" :value='this.text' v-on:click='this.clickOn' />
+    <div class="container">
+        <h1> Search Recipes</h1>
+        <div class='is-icon is-searchPage' v-on:click='focus()'></div>
+        <input class='is-field is-searchPage' type='text' ref='input' v-model='search_text' />
+        <input class="is-button is-search-button" type="button" :value='this.text' v-on:click='searchRecipes' />
+        <div>
+            <b-form-select v-model="selected" :options="options" size="sm" class="mt-3" ></b-form-select>
+            <input type="checkbox" id="cuisine" v-model="checked">
+            <label for="cuisine">cuisine</label>
+        </div>
+        <search-result></search-result>
     </div>
 </template>
 
 <script>
+    import SearchResult from "../components/SearchResult";
     export default {
-      name : 'searchbox',
+        name : 'searchPage',
+        components: {SearchResult},
+        data(){
+            return{
+                search_text: "",
+                recipe:[],
+                selected: null,
+                options: [
+                    { value: null, text: 'Please select number of recipes' },
+                    { value: '5', text: '5' },
+                    { value: '10', text: '10' },
+                    { value: '15', text: '15' }
+                ],
+                checked:[]
+            }
+        },
 
-      props:{
-        "text":{
-          type: String,
-          default : "Search"
+        props:{
+            "text":{
+                type: String,
+                default : "Search"
+            }
+        },
 
-        }
-      },
+        methods : {
+            clickOn : function() {
+                // Execute request
+                this.fetch();
+            },
 
-      methods : {
-        clickOn : function() {
-          // Execute request
-          this.fetch();
-        }
-      },
+            async searchRecipes() {
+                try {
+                    const response = await this.axios.get(
+                        "http://loclahost:3000/search/query/",
+                        {
+                            searchQuery: this.search_text,
+                            num: this.selected
+                        }
+                        //     "https://test-for-3-2.herokuapp.com/recipes/random"
+                    );
+
+                    console.log(response);
+                    const recipes = response.data;
+                    this.recipes = [];
+                    this.recipes.push(...recipes);
+                    this.num.push(...this.selected);
+                    // console.log(this.recipes);
+                } catch (error) {
+                    console.log(error);
+                }
+            }
+        },
 
     }
 </script>
 
-<style scoped>
-
+<style lang="scss" scoped>
+    .container {
+        align-items: center;
+        max-width: 400px;
+    }
 </style>
