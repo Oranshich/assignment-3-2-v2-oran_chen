7955dc22e51b0649bfcc4b2896dfa070f7797886
Chen2908 <chengal@post.bgu.ac.il>
2020-06-21 17:10:02 +0300

    favorites page

---
+++ src/components/RecipePreviewResults.vue
@@ -0,0 +1,36 @@
+<template>
+    <b-container>
+        <h3>
+            {{ title }}:
+        </h3>
+        <b-row>
+            <b-col v-for="r in recipes" :key="r.id">
+                <RecipePreview class="recipePreview" :recipe="r" />
+            </b-col>
+        </b-row>
+    </b-container>
+</template>
+<script>
+    import RecipePreview from "./RecipePreview";
+
+    export default {
+        name: "RecipePreviewResults",
+        components: {
+            RecipePreview
+        },
+        props: {
+            title: {
+                type: String,
+                required: true
+            },
+            recipes: {
+                type: Array,
+                required: true
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
